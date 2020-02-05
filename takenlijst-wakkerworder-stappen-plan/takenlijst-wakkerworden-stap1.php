eba9ed9ceea3e8ac70b802164e66a7fe601c8405
noxion1 <43441327+noxion1@users.noreply.github.com>
2020-02-05 18:41:32 +0100

    Add files via upload

---
+++ takenlijst-wakkerworder-stappen-plan/takenlijst-wakkerworden-stap1.php
@@ -0,0 +1,82 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="lege__ruimte">
+
+    </div>
+    <div class="kkrgroteborder">
+
+    <ul>
+  <li class="nav__a--border"><a href="paniek.php">paniek</a></li>
+  <li class="nav__a--border"><a href="index.php">uitloggen</a></li>
+  <li class="nav__a--border"><a href="javascript:history.go(-1)">terug</a></li>
+</ul>
+  <hr>
+
+<div class="taak2">
+
+  <center><h1>Wakker worden</h1></center>
+
+  <p class="takenlijst__wakkerworden--stap1">Stap 1</p>
+
+  <p class="takenlijst__wakkerworden--info">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ Aenean euismod bibendum laoreet.
+ Proin gravida dolor sit amet
+ lacus accumsan et viverra justo
+ commodo. Proin sodales pulvinar
+ sic tempor. roin gravida dolor sit amet
+ lacus accumsan et viverra justo
+ commodo. roin gravida dolor sit.
+ commodo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Aenean euismod bibendum.
+  </p>
+
+  <a href="takenlijst-wakkerworden-stap2.php">
+  <div class="takenlijst__wakkerworden--stap--verder">
+<p style="font-size:22px; text-align:center; background-color: #E8BDFE;">Stap 2</p>
+  </div>
+  </a>
+
+</div>
+
+<div class="taak2">
+
+  <div class="buttonkutwaar">
+  </div>
+
+  <div class="button play"></div>
+
+  <div class="buttonkut"></div>
+
+<br><br><br><br><br><br>
+
+
+
+<div class="takenlijst__wakkerworden--audio--streepje-links">
+
+</div>
+
+<div class="takenlijst__wakkerworden--audio--streepje-rechts">
+
+</div>
+
+<div class="takenlijst__wakkerworden--audio">
+
+</div>
+
+<p class="takenlijst__wakkerworden--audio--begintijd">00:00</p>
+
+<p class="takenlijst__wakkerworden--audio--eindtijd">05:00</p>
+
+<br><br>
+</div>
+
+
+</div>
+</body>
+</html>
---
+++ takenlijst-wakkerworder-stappen-plan/takenlijst-wakkerworden-stap4.php
@@ -0,0 +1,85 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="../css/style.css" rel="stylesheet">
+  </head>
+  <body>
+    <div class="lege__ruimte">
+
+    </div>
+    <div class="kkrgroteborder">
+
+    <ul>
+  <li class="nav__a--border"><a href="paniek.php">paniek</a></li>
+  <li class="nav__a--border"><a href="index.php">uitloggen</a></li>
+  <li class="nav__a--border"><a href="javascript:history.go(-1)">terug</a></li>
+</ul>
+  <hr>
+
+<div class="taak2">
+
+  <center><h1>Wakker worden</h1></center>
+
+  <p class="takenlijst__wakkerworden--stap1">Stap 4</p>
+
+  <p class="takenlijst__wakkerworden--info">
+    Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+ Aenean euismod bibendum laoreet.
+ Proin gravida dolor sit amet
+ lacus accumsan et viverra justo
+ commodo. Proin sodales pulvinar
+ sic tempor. roin gravida dolor sit amet
+ lacus accumsan et viverra justo
+ commodo. roin gravida dolor sit.
+ commodo. Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Aenean euismod bibendum.
+  </p>
+
+</div>
+
+<div class="taak2">
+
+  <div class="buttonkutwaar">
+  </div>
+
+  <div class="button play"></div>
+
+  <div class="buttonkut"></div>
+
+<br><br><br><br><br><br>
+
+
+
+<div class="takenlijst__wakkerworden--audio--streepje-links">
+
+</div>
+
+<div class="takenlijst__wakkerworden--audio--streepje-rechts">
+
+</div>
+
+<div class="takenlijst__wakkerworden--audio">
+
+</div>
+
+<p class="takenlijst__wakkerworden--audio--begintijd">00:00</p>
+
+<p class="takenlijst__wakkerworden--audio--eindtijd">05:00</p>
+
+<br><br>
+
+<a href="../takenlijst.php">
+<div class="takenlijst__wakkerworden--gedaan-stap4">
+✔
+</div>
+</a>
+
+
+</div>
+
+<br><br><br><br>
+
+</div>
+</body>
+</html>
